6778c0ec67f618b22b3604e44a83548d5a10f53f
Benedict <newlyf38@gmail.com>
2023-11-20 13:40:27 +0000

    added

--- ug_scholar/settings.py
+++ ug_scholar/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["vercel.app"]
 
 
 # Application definition
